79419cdde9cb2916504251916648fc703d7dfc10
fangzhipeng <124746406@qq.com>
2019-01-31 17:10:10 +0800

    consul

---
+++ chapter-consul/consul-provider/src/main/java/com/forezp/ConsulProviderApplication.java
@@ -0,0 +1,20 @@
+package com.forezp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+
+
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ConsulProviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsulProviderApplication.class, args);
+	}
+
+}
+
+

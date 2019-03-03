858ee37fc534e493a3bb078aabee3e607fd6e419
fangzhipeng <fangzhipeng@xiaoantimes.com>
2019-03-03 20:02:13 +0800

    admin

--- chapter12-2/eureka-server/src/main/java/com/forezp/eurekaserver/EurekaServerApplication.java
+++ chapter12-2/eureka-server/src/main/java/com/forezp/eurekaserver/EurekaServerApplication.java
@@ -4,10 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-/**
- * 重要的事情说三遍：
- * 文档地址： http://cloud.spring.io/spring-cloud-static/Finchley.RELEASE/single/spring-cloud.html
- */
+
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaServerApplication {
--- chapter12/admin-client/src/main/java/com/forezp/adminclient/AdminClientApplication.java
+++ chapter12/admin-client/src/main/java/com/forezp/adminclient/AdminClientApplication.java
@@ -6,9 +6,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @SpringBootApplication
 @RestController
@@ -21,14 +18,7 @@ public class AdminClientApplication {
     }
 
 
-//    @Configuration
-//    public static class SecurityPermitAllConfig extends WebSecurityConfigurerAdapter {
-//        @Override
-//        protected void configure(HttpSecurity http) throws Exception {
-//            http.authorizeRequests().anyRequest().permitAll()
-//                    .and().csrf().disable();
-//        }
-//    }
+
 
     @GetMapping("/test")
     public String test() {

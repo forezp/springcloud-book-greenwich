661d56d0b92f6892f3b55592e62d49f39c79432f
fangzhipeng <124746406@qq.com>
2019-03-19 18:55:04 +0800

    sc book

---
+++ sc-f-gateway-predicate/src/main/java/gateway/Application.java
@@ -0,0 +1,45 @@
+package gateway;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.springframework.web.bind.annotation.RestController;
+
+// tag::code[]
+@SpringBootApplication
+@RestController
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+
+//    @Bean
+//    public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+//        String httpUri = "http://httpbin.org:80";
+//        return builder.routes()
+//            .route(p -> p
+//                .path("/get")
+//                .filters(f -> f.addRequestHeader("Hello", "World"))
+//                .uri(httpUri))
+//            .route(p -> p
+//                .host("*.hystrix.com")
+//                .filters(f -> f
+//                    .hystrix(config -> config
+//                        .setName("mycmd")
+//                        .setFallbackUri("forward:/fallback")))
+//                .uri(httpUri))
+//            .build();
+//    }
+    // end::route-locator[]
+
+    // tag::fallback[]
+//    @RequestMapping("/fallback")
+//    public Mono<String> fallback() {
+//        return Mono.just("fallback");
+//    }
+
+}
+
